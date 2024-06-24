bf7e81b555a55617dbfa57df16007a73947ea15a
YuPing <yupingw95@gmail.com>
2024-06-24 18:23:16 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import formatCurrency from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -115,9 +116,10 @@ export function renderOrderSummary(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);   
+
         container.remove();
+        renderPaymentSummary();
       });
   });
 
@@ -127,6 +129,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
   });
 }
