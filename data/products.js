f107e2d6b0b32aa5dc042b6e02083926295a5eaa
YuPing <yupingw95@gmail.com>
2024-06-26 12:21:10 +0800

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -91,6 +91,29 @@ obj.method();
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
@@ -103,7 +126,7 @@ export function loadProducts(fun){
     });
 
     console.log('load products');
-    
+
     fun();
   })
 
