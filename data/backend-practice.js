6fb22b5c3ad9e5060fffc03d0ba4d1f95e808549
YuPing <yupingw95@gmail.com>
2024-06-26 10:31:58 +0800

    Add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,9 @@
+/* Using the browser = making a GET request */
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.response);
+})
+xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.send();
+
